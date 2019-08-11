a1b1888b58bda7b522a877a847d4c760a3057b9e
adrian hernandez <adrianhh1426@gmail.com>
2019-08-11 14:32:12 -0500

    efecto thunk final

--- reactProyecto/hola_mundo/src/Thunk.js
+++ reactProyecto/hola_mundo/src/Thunk.js
@@ -1,5 +1,66 @@
+//action types
+const FETCH_START="start";
+const FETCH_SUCCESS="success";
+const FETCH_ERROR="error";
+
+//action creator
+const fetchStart = () =>({
+    type:FETCH_START
+});
+
+const fetchSuccess = payload =>({
+    type:FETCH_SUCCESS,
+    payload
+});
+
+const fetchError = error =>({
+    type:FETCH_ERROR,
+    error
+});
+
+const url="https://jsonplaceholder.typicode.com/users";
+
+const initialState={
+    data:[],
+    fetching:false,
+    error:null
+}
+
+//reducer
+export function reducer (state=initialState,action){
+    console.log(action);
+    switch (action.type) {
+        case FETCH_START:
+            return{
+                ...state,
+                fetching:true
+            }
+        case FETCH_SUCCESS:
+            return{
+                ...state,
+                data:action.payload
+            }
+        case FETCH_ERROR:
+            return{
+                ...state,
+                error:action.error
+            }
+        default:
+            return state;
+    }
+    
+}
+
 export default payload =>
-(dispatch,getState)=>{
-    const state=getState()
-    console.log(state)
+async (dispatch,getState)=>{
+    dispatch(fetchStart());
+
+    try {
+        const result= await fetch(url);
+        const json= await result.json();
+        dispatch(fetchSuccess(json));   
+        console.log("json: ",json);
+    } catch (error) {
+        dispatch(fetchError(error));
+    }
 }
--- reactProyecto/hola_mundo/src/index.js
+++ reactProyecto/hola_mundo/src/index.js
@@ -6,14 +6,16 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import {reducer} from './Thunk';
 
-const initialState ={
-    data:[1,2,3],
-    selected:1
-}
-function reducer (state=initialState,action){
-    return state;
-}
+// const initialState ={
+//     data:[1,2,3],
+//     selected:1
+// }
+// function reducer (state=initialState,action){
+//     console.log("action disparando: ",action);
+//     return state;
+// }
 const store= createStore(reducer,applyMiddleware(thunk));
 
 ReactDOM.render(
